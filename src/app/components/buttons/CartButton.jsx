a6ba271fa3a6e0a44a77c7ebb14dfa8c014cf651
AhmedYeasin <ahmetchatgami@gmail.com>
2026-03-06 23:20:08 +0600

    cart items successfully added to DB

--- src/app/components/buttons/CartButton.jsx
+++ src/app/components/buttons/CartButton.jsx
@@ -3,32 +3,37 @@
 import { handleCart } from "@/actions/server/cart";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import Swal from "sweetalert2";
 
 const CartButton = ({ product }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const session = useSession();
   const isLogin = session?.status == "authenticated";
   const router = useRouter();
   const path = usePathname();
 
   const addtoCart = async () => {
+    setIsLoading(true);
     if (isLogin) {
-      const result= await handleCart({ product, inc: true });
-      if(result.success){
-        Swal.fire("Added to Cart", product.title, "success")
-      } else{
-        Swal.fire("Oops!", "Something Went Wrong", "error")
-
+      const result = await handleCart({ product, inc: true });
+      if (result.success) {
+        Swal.fire("Added to Cart", product.title, "success");
+      } else {
+        Swal.fire("Oops!", "Something Went Wrong", "error");
       }
+      setIsLoading(false);
     } else {
       router.push(`/login?callbackUrl=${path}`);
+      setIsLoading(false)
     }
   };
   return (
     <div>
-      <button onClick={addtoCart} className="btn btn-primary gap-2">
+      <button 
+      disabled = {session.status == "loading" || isLoading}
+      onClick={addtoCart} className="btn btn-primary gap-2">
         <FaShoppingCart />
         Add to Cart
       </button>
